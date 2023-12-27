898ffb0a114981a7af48a2bf95ab566c0b37825a
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2023-12-27 11:57:26 +0545

    update of pricing page

--- Agriconnect/urls.py
+++ Agriconnect/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,pricing
 
 urlpatterns = [
     path('',index),
+    path('index',index),
+    path('pricing',pricing),
     path('admin/', admin.site.urls),
     
 ]
--- Agriconnect/views.py
+++ Agriconnect/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def pricing(request):
+    return render(request,'pricing.html')
