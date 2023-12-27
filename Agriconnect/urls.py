eb94307866919f750c0693ee8d1721769e73e2cc
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2023-12-27 10:47:15 +0545

    small change in setup

--- Agriconnect/urls.py
+++ Agriconnect/urls.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import index
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',index),
+    path('admin/', admin.site.urls),
     
 ]
